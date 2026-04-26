b6d774be2a850acfe87c5dd3b25a458812132b5c
Shakhawat <srabonshakhawat@gmail.com>
2026-04-27 05:43:14 +0600

    Update user

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 const createUserZodSchema = z.object({
   body: z.object({
     name: z.string({ required_error: 'Name is required' }),
-    contact: z.string({ required_error: 'Contact is required' }),
     email: z.string({ required_error: 'Email is required' }),
     password: z.string({ required_error: 'Password is required' }),
     profile: z.string().optional(),
@@ -12,7 +11,6 @@ const createUserZodSchema = z.object({
 
 const updateUserZodSchema = z.object({
   name: z.string().optional(),
-  contact: z.string().optional(),
   email: z.string().optional(),
   password: z.string().optional(),
   image: z.string().optional(),
